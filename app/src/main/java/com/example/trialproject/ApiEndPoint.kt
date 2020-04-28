ba0f3fd0b0891edd3d80c382e86c8f11016ca2fa
ramaarief <ramaarief45@gmail.com>
2020-04-28 22:13:05 +0700

    Update class ApiEndPoint

--- app/src/main/java/com/example/trialproject/ApiEndPoint.kt
+++ app/src/main/java/com/example/trialproject/ApiEndPoint.kt
@@ -3,8 +3,27 @@ package com.example.trialproject
 class ApiEndPoint {
     companion object {
 
-        private val SERVER = "http://192.168.1.3/sektor2/"
+        private val SERVER = "http://192.168.1.6/sektor2/"
         val READ = SERVER+"perusahaan_json.php"
+
+        val perusahaan1 = SERVER+"perusahaan_1.php"
+        val perusahaan2 = SERVER+"perusahaan_2.php"
+        val perusahaan3 = SERVER+"perusahaan_3.php"
+        val perusahaan4 = SERVER+"perusahaan_4.php"
+        val perusahaan5 = SERVER+"perusahaan_5.php"
+        val perusahaan6 = SERVER+"perusahaan_6.php"
+        val perusahaan7 = SERVER+"perusahaan_7.php"
+        val perusahaan8 = SERVER+"perusahaan_8.php"
+        val perusahaan9 = SERVER+"perusahaan_9.php"
+        val perusahaan10 = SERVER+"perusahaan_10.php"
+
+        val sektor1 = SERVER+"sektor_1.php"
+        val sektor2 = SERVER+"sektor_2.php"
+        val sektor3 = SERVER+"sektor_3.php"
+        val sektor4 = SERVER+"sektor_4.php"
+        val sektor5 = SERVER+"sektor_5.php"
+        val sektor6 = SERVER+"sektor_6.php"
+        val sektor7 = SERVER+"sektor_7.php"
 
     }
 }
